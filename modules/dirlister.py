08950cff4f93ff99fe2333f9321ea27d0163aadc
Peder Lind Mangset <peder.mangset@gmail.com>
2017-11-25 10:22:34 -0500

    Adding new modules

---
+++ modules/dirlister.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+import os
+
+def run(**args):
+    print("[*] In direlister module.")
+    files = os.listdir(".")
+    return str(files)
---
+++ modules/enviroment.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+import os
+
+def run(**args):
+   print("[*] In enviroment module.")
+   return str(os.environ)
